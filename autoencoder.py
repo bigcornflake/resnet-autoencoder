7d60eec4d3dd15c70a6bfe6b9eef34f11ede9f7c
big cornflake <3396323+bigcornflake@users.noreply.github.com>
2024-01-06 23:15:45 +0800

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -1,341 +1,181 @@
+import urllib.parse
+from argparse import ArgumentParser
+
 import torch
+from pytorch_lightning import LightningModule, Trainer
 from torch import nn
-from torch.nn import functional as F  # noqa: N812
-
-from pl_bolts.utils.stability import under_review
+from torch.nn import functional as F
 
 
 @under_review()
-class Interpolate(nn.Module):
-    """nn.Module wrapper for F.interpolate."""
+class AE(LightningModule):
+    """Standard AE.
 
-    def __init__(self, size=None, scale_factor=None) -> None:
-        super().__init__()
-        self.size, self.scale_factor = size, scale_factor
+    Model is available pretrained on different datasets:
 
-    def forward(self, x):
-        return F.interpolate(x, size=self.size, scale_factor=self.scale_factor)
+    Example::
 
+        # not pretrained
+        ae = AE()
 
-@under_review()
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding."""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        # pretrained on cifar10
+        ae = AE(input_height=32).from_pretrained('cifar10-resnet18')
 
+    """
 
-@under_review()
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution."""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+    pretrained_urls = {
+        "cifar10-resnet18": urllib.parse.urljoin(_HTTPS_AWS_HUB, "ae/ae-cifar10/checkpoints/epoch%3D96.ckpt"),
+    }
 
+    def __init__(
+        self,
+        input_height: int,
+        enc_type: str = "resnet18",
+        first_conv: bool = False,
+        maxpool1: bool = False,
+        enc_out_dim: int = 512,
+        latent_dim: int = 256,
+        lr: float = 1e-4,
+        **kwargs,
+    ) -> None:
+        """
+        Args:
+            input_height: height of the images
+            enc_type: option between resnet18 or resnet50
+            first_conv: use standard kernel_size 7, stride 2 at start or
+                replace it with kernel_size 3, stride 1 conv
+            maxpool1: use standard maxpool to reduce spatial dim of feat by a factor of 2
+            enc_out_dim: set according to the out_channel count of
+                encoder used (512 for resnet18, 2048 for resnet50)
+            latent_dim: dim of latent space
+            lr: learning rate for Adam
+        """
 
-@under_review()
-def resize_conv3x3(in_planes, out_planes, scale=1):
-    """Upsample + 3x3 convolution with padding to avoid checkerboard artifact."""
-    if scale == 1:
-        return conv3x3(in_planes, out_planes)
-    return nn.Sequential(Interpolate(scale_factor=scale), conv3x3(in_planes, out_planes))
-
-
-@under_review()
-def resize_conv1x1(in_planes, out_planes, scale=1):
-    """Upsample + 1x1 convolution with padding to avoid checkerboard artifact."""
-    if scale == 1:
-        return conv1x1(in_planes, out_planes)
-    return nn.Sequential(Interpolate(scale_factor=scale), conv1x1(in_planes, out_planes))
-
-
-@under_review()
-class EncoderBlock(nn.Module):
-    """ResNet block, copied from https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py#L35."""
-
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None) -> None:
-        super().__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        return self.relu(out)
-
-
-@under_review()
-class EncoderBottleneck(nn.Module):
-    """ResNet bottleneck, copied from
-    https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py#L75."""
-
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None) -> None:
-        super().__init__()
-        width = planes  # this needs to change if we want wide resnets
-        self.conv1 = conv1x1(inplanes, width)
-        self.bn1 = nn.BatchNorm2d(width)
-        self.conv2 = conv3x3(width, width, stride)
-        self.bn2 = nn.BatchNorm2d(width)
-        self.conv3 = conv1x1(width, planes * self.expansion)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        return self.relu(out)
-
-
-@under_review()
-class DecoderBlock(nn.Module):
-    """ResNet block, but convs replaced with resize convs, and channel increase is in second conv, not first."""
-
-    expansion = 1
-
-    def __init__(self, inplanes, planes, scale=1, upsample=None) -> None:
-        super().__init__()
-        self.conv1 = resize_conv3x3(inplanes, inplanes)
-        self.bn1 = nn.BatchNorm2d(inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = resize_conv3x3(inplanes, planes, scale)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.upsample = upsample
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.upsample is not None:
-            identity = self.upsample(x)
-
-        out += identity
-        return self.relu(out)
-
-
-@under_review()
-class DecoderBottleneck(nn.Module):
-    """ResNet bottleneck, but convs replaced with resize convs."""
-
-    expansion = 4
-
-    def __init__(self, inplanes, planes, scale=1, upsample=None) -> None:
-        super().__init__()
-        width = planes  # this needs to change if we want wide resnets
-        self.conv1 = resize_conv1x1(inplanes, width)
-        self.bn1 = nn.BatchNorm2d(width)
-        self.conv2 = resize_conv3x3(width, width, scale)
-        self.bn2 = nn.BatchNorm2d(width)
-        self.conv3 = conv1x1(width, planes * self.expansion)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.upsample = upsample
-        self.scale = scale
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.upsample is not None:
-            identity = self.upsample(x)
-
-        out += identity
-        return self.relu(out)
-
-
-@under_review()
-class ResNetEncoder(nn.Module):
-    def __init__(self, block, layers, first_conv=False, maxpool1=False) -> None:
         super().__init__()
 
-        self.inplanes = 64
-        self.first_conv = first_conv
-        self.maxpool1 = maxpool1
+        self.save_hyperparameters()
 
-        if self.first_conv:
-            self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
-        else:
-            self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
-
-        self.bn1 = nn.BatchNorm2d(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
-
-        if self.maxpool1:
-            self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        else:
-            self.maxpool = nn.MaxPool2d(kernel_size=1, stride=1)
-
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        return torch.flatten(x, 1)
-
-
-@under_review()
-class ResNetDecoder(nn.Module):
-    """Resnet in reverse order."""
-
-    def __init__(self, block, layers, latent_dim, input_height, first_conv=False, maxpool1=False) -> None:
-        super().__init__()
-
-        self.expansion = block.expansion
-        self.inplanes = 512 * block.expansion
-        self.first_conv = first_conv
-        self.maxpool1 = maxpool1
+        self.lr = lr
+        self.enc_out_dim = enc_out_dim
+        self.latent_dim = latent_dim
         self.input_height = input_height
 
-        self.upscale_factor = 8
+        valid_encoders = {
+            "resnet18": {
+                "enc": resnet18_encoder,
+                "dec": resnet18_decoder,
+            },
+            "resnet50": {
+                "enc": resnet50_encoder,
+                "dec": resnet50_decoder,
+            },
+        }
 
-        self.linear = nn.Linear(latent_dim, self.inplanes * 4 * 4)
-
-        self.layer1 = self._make_layer(block, 256, layers[0], scale=2)
-        self.layer2 = self._make_layer(block, 128, layers[1], scale=2)
-        self.layer3 = self._make_layer(block, 64, layers[2], scale=2)
-
-        if self.maxpool1:
-            self.layer4 = self._make_layer(block, 64, layers[3], scale=2)
-            self.upscale_factor *= 2
+        if enc_type not in valid_encoders:
+            self.encoder = resnet18_encoder(first_conv, maxpool1)
+            self.decoder = resnet18_decoder(self.latent_dim, self.input_height, first_conv, maxpool1)
         else:
-            self.layer4 = self._make_layer(block, 64, layers[3])
+            self.encoder = valid_encoders[enc_type]["enc"](first_conv, maxpool1)
+            self.decoder = valid_encoders[enc_type]["dec"](self.latent_dim, self.input_height, first_conv, maxpool1)
 
-        if self.first_conv:
-            self.upscale = Interpolate(scale_factor=2)
-            self.upscale_factor *= 2
-        else:
-            self.upscale = Interpolate(scale_factor=1)
+        self.fc = nn.Linear(self.enc_out_dim, self.latent_dim)
 
-        # interpolate after linear layer using scale factor
-        self.upscale1 = Interpolate(size=input_height // self.upscale_factor)
+    @staticmethod
+    def pretrained_weights_available():
+        return list(AE.pretrained_urls.keys())
 
-        self.conv1 = nn.Conv2d(64 * block.expansion, 3, kernel_size=3, stride=1, padding=1, bias=False)
+    def from_pretrained(self, checkpoint_name):
+        if checkpoint_name not in AE.pretrained_urls:
+            raise KeyError(str(checkpoint_name) + " not present in pretrained weights.")
 
-    def _make_layer(self, block, planes, blocks, scale=1):
-        upsample = None
-        if scale != 1 or self.inplanes != planes * block.expansion:
-            upsample = nn.Sequential(
-                resize_conv1x1(self.inplanes, planes * block.expansion, scale),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, scale, upsample))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
+        return self.load_from_checkpoint(AE.pretrained_urls[checkpoint_name], strict=False)
 
     def forward(self, x):
-        x = self.linear(x)
+        feats = self.encoder(x)
+        z = self.fc(feats)
+        return self.decoder(z)
 
-        # NOTE: replaced this by Linear(in_channels, 514 * 4 * 4)
-        # x = F.interpolate(x, scale_factor=4)
+    def step(self, batch, batch_idx):
+        x, y = batch
 
-        x = x.view(x.size(0), 512 * self.expansion, 4, 4)
-        x = self.upscale1(x)
+        feats = self.encoder(x)
+        z = self.fc(feats)
+        x_hat = self.decoder(z)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.upscale(x)
+        loss = F.mse_loss(x_hat, x, reduction="mean")
 
-        return self.conv1(x)
+        return loss, {"loss": loss}
+
+    def training_step(self, batch, batch_idx):
+        loss, logs = self.step(batch, batch_idx)
+        self.log_dict({f"train_{k}": v for k, v in logs.items()}, on_step=True, on_epoch=False)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss, logs = self.step(batch, batch_idx)
+        self.log_dict({f"val_{k}": v for k, v in logs.items()})
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+
+        parser.add_argument("--enc_type", type=str, default="resnet18", help="resnet18/resnet50")
+        parser.add_argument("--first_conv", action="store_true")
+        parser.add_argument("--maxpool1", action="store_true")
+        parser.add_argument("--lr", type=float, default=1e-4)
+
+        parser.add_argument(
+            "--enc_out_dim",
+            type=int,
+            default=512,
+            help="512 for resnet18, 2048 for bigger resnets, adjust for wider resnets",
+        )
+        parser.add_argument("--latent_dim", type=int, default=256)
+
+        parser.add_argument("--batch_size", type=int, default=256)
+        parser.add_argument("--num_workers", type=int, default=8)
+        parser.add_argument("--data_dir", type=str, default=".")
+
+        return parser
 
 
 @under_review()
-def resnet18_encoder(first_conv, maxpool1):
-    return ResNetEncoder(EncoderBlock, [2, 2, 2, 2], first_conv, maxpool1)
+def cli_main(args=None):
+    from pl_bolts.datamodules import CIFAR10DataModule, ImagenetDataModule, STL10DataModule
+
+    parser = ArgumentParser()
+    parser.add_argument("--dataset", default="cifar10", type=str, choices=["cifar10", "stl10", "imagenet"])
+    script_args, _ = parser.parse_known_args(args)
+
+    if script_args.dataset == "cifar10":
+        dm_cls = CIFAR10DataModule
+    elif script_args.dataset == "stl10":
+        dm_cls = STL10DataModule
+    elif script_args.dataset == "imagenet":
+        dm_cls = ImagenetDataModule
+    else:
+        raise ValueError(f"undefined dataset {script_args.dataset}")
+
+    parser = AE.add_model_specific_args(parser)
+    parser = Trainer.add_argparse_args(parser)
+    args = parser.parse_args(args)
+
+    dm = dm_cls.from_argparse_args(args)
+    args.input_height = dm.dims[-1]
+
+    if args.max_steps == -1:
+        args.max_steps = None
+
+    model = AE(**vars(args))
+
+    trainer = Trainer.from_argparse_args(args)
+    trainer.fit(model, datamodule=dm)
+    return dm, model, trainer
 
 
-@under_review()
-def resnet18_decoder(latent_dim, input_height, first_conv, maxpool1):
-    return ResNetDecoder(DecoderBlock, [2, 2, 2, 2], latent_dim, input_height, first_conv, maxpool1)
-
-
-@under_review()
-def resnet50_encoder(first_conv, maxpool1):
-    return ResNetEncoder(EncoderBottleneck, [3, 4, 6, 3], first_conv, maxpool1)
-
-
-@under_review()
-def resnet50_decoder(latent_dim, input_height, first_conv, maxpool1):
-    return ResNetDecoder(DecoderBottleneck, [3, 4, 6, 3], latent_dim, input_height, first_conv, maxpool1)
+if __name__ == "__main__":
+    dm, model, trainer = cli_main()
